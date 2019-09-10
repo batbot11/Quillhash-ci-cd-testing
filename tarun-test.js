97e0ca06b6bd9cdbeada8cb30f85a40e9230d185
Tarun Pathak <pathaktarun1111@gmail.com>
2019-09-10 14:48:21 +0530

    New console.log aded

--- tarun-test.js
+++ tarun-test.js
@@ -8,3 +8,5 @@ console.log(" xwjd webjd ewhd wed ew dhwe dhgew dgh weghd w");
 console.log("s ewdweghdewghderdgf erfefegf erf erf erfefbefvr");
 
 console.log("bdhwvedg3vfg3rvfguv3rgfuv3uf");
+
+console.log("dbhewdb3vdbj3vdg3dvu34dvh2vedgu3dvuy32d");
