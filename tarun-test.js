375866c15af90260bae64d8784fb23dd9ea7c649
Tarun Pathak <pathaktarun1111@gmail.com>
2019-09-13 12:41:35 +0530

    3 Console log removed

--- tarun-test.js
+++ tarun-test.js
@@ -1,11 +1,5 @@
 console.log("This file is for testing purposes only");
 
-console.log("s ewdweghdewghderdgf erfefegf erf erf erfefbefvr");
-
-console.log("bdhwvedg3vfg3rvfguv3rgfuv3uf");
-
-console.log("dbhewdb3vdbj3vdg3dvu34dvh2vedgu3dvuy32d");
-
 console.log("ebhwebdeubddbeubduebdyub2d");
 
 console.log("2bdhwjebdhjrbvfjegfv jefvgegfvegf");
