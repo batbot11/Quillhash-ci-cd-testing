dbd54ff900732db4649107dd833a674319d0bd73
Tarun Pathak <pathaktarun1111@gmail.com>
2019-09-16 11:46:09 +0530

    3 console logs added

--- tarun-test.js
+++ tarun-test.js
@@ -3,3 +3,9 @@ console.log("This file is for testing purposes only");
 console.log("ebhwebdeubddbeubduebdyub2d");
 
 console.log("2bdhwjebdhjrbvfjegfv jefvgegfvegf");
+
+console.log(" jwed ewhjbdehbdewbdewbdewbjhdbewjdb");
+
+console.log("bshebdehdbehdbwejdb32jdbe2hjbde3bdhb");
+
+console.log("vdwehdvwegdvwejdbewhjdbwehjdvewjdvewgjvdj");
