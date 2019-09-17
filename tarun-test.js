e404b69087c74fdc97b6d04c1bea51d8d93bed31
Tarun Pathak <pathaktarun1111@gmail.com>
2019-09-17 13:26:17 +0530

    console logs removed

--- tarun-test.js
+++ tarun-test.js
@@ -6,14 +6,6 @@ console.log("2bdhwjebdhjrbvfjegfv jefvgegfvegf");
 
 console.log(" jwed ewhjbdehbdewbdewbdewbjhdbewjdb");
 
-console.log("bshebdehdbehdbwejdb32jdbe2hjbde3bdhb");
 
-console.log("vdwehdvwegdvwejdbewhjdbwehjdvewjdvewgjvdj");
-
-console.log("vgqwedveghwdvewghdvewghdvewghdvgewvdghvj h");
-
-console.log("sedkewbhdbewhjdbewhjd bwehjdbwhdbewhjwbjh");
-
-console.log(" sjqwdwgehdvewghdvewghdvewghvdewgvdewghvdghwvdghvew");
 
 console.log("edbhjdbwehjdbwhjedbewhjbdhewjbdwehjbdhewbdhjewbdewb");
