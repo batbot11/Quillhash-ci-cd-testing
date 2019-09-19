c66516f675be60f5b9e1568ab8451f272d1310ce
Tarun Pathak <pathaktarun1111@gmail.com>
2019-09-19 13:09:54 +0530

    Bug in commit details page resolved

--- tarun-test.js
+++ tarun-test.js
@@ -11,3 +11,5 @@ console.log("edbhjdbwehjdbwhjedbewhjbdhewjbdwehjbdhewbdhjewbdewb");
 console.log("bshbsh2jbs23hjbshj23bs32hjsb32bshb32hjsb32hjsb");
 
 console.log("dewndkjendjkewndkjwnedjkwebdv ewghd wegd ewgdged");
+
+console.log("ebdhewdbwehjdbehjdbehjdbe2hjdbe2hjdbedbje3hbdh3ejbdhj");
