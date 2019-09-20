8e0e7b24ae4c06e90ab16ec83f40675219c4a8be
Tarun Pathak <pathaktarun1111@gmail.com>
2019-09-20 18:05:47 +0530

    Login view of the app altered

--- tarun-test.js
+++ tarun-test.js
@@ -9,11 +9,3 @@ console.log(" jwed ewhjbdehbdewbdewbdewbjhdbewjdb");
 console.log("edbhjdbwehjdbwhjedbewhjbdhewjbdwehjbdhewbdhjewbdewb");
 
 console.log("bshbsh2jbs23hjbshj23bs32hjsb32bshb32hjsb32hjsb");
-
-console.log("dewndkjendjkewndkjwnedjkwebdv ewghd wegd ewgdged");
-
-console.log("ebdhewdbwehjdbehjdbehjdbe2hjdbe2hjdbedbje3hbdh3ejbdhj");
-
-console.log("edhjewbdewhjdbehbdhewjdbe2hbehbde3hbde3bdhbe2hjdbe3bd");
-
-console.log("bhjqwbwehjdbewhjdbwehjdbjewbdewhbdhewbdbwedbewjdjbwe")
