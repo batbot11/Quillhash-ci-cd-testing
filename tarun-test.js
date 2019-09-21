425ecba42871e4f45879e3cb71f8c76f79b3a0e0
Tarun Pathak <pathaktarun1111@gmail.com>
2019-09-21 15:19:56 +0530

    Problem on loader removed

--- tarun-test.js
+++ tarun-test.js
@@ -11,3 +11,5 @@ console.log("edbhjdbwehjdbwhjedbewhjbdhewjbdwehjbdhewbdhjewbdewb");
 console.log("bshbsh2jbs23hjbshj23bs32hjsb32bshb32hjsb32hjsb");
 
 console.log("dvedbedbhejdbh3jdbhe3jbd3hjedbhe3bdjhbe3djehbde3bdhj");
+
+console.log("bdhebdwehdbwehjdwehjdbewjhbewhjdbwehjdbhjewdbewhjdbewhjdbwehj");
