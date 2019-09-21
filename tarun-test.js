41b69d16116c9614052d7ebecac3609a2734a708
Tarun Pathak <pathaktarun1111@gmail.com>
2019-09-21 12:55:46 +0530

    Unauthorized access to inner pages not allowed

--- tarun-test.js
+++ tarun-test.js
@@ -9,3 +9,5 @@ console.log(" jwed ewhjbdehbdewbdewbdewbjhdbewjdb");
 console.log("edbhjdbwehjdbwhjedbewhjbdhewjbdwehjbdhewbdhjewbdewb");
 
 console.log("bshbsh2jbs23hjbshj23bs32hjsb32bshb32hjsb32hjsb");
+
+console.log("dvedbedbhejdbh3jdbhe3jbd3hjedbhe3bdjhbe3djehbde3bdhj");
